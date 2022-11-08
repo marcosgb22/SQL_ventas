e11e474a825be6f594a5dc333c05658d17d79d46
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-08 17:15:58 -0300

    Update inserciones_de_datos.sql

--- inserciones_de_datos.sql
+++ inserciones_de_datos.sql
@@ -1,1 +1,13 @@
 
+
+INSERT INTO `USUARIOS` (`ID_USUARIOS`,`NOMBRE`,`CORREO`,`DIRECCION`,`TELEFONO`)
+VALUES
+  (1,"Daryl Garrett","in@protonmail.couk","Ap #425-3654 Mi Rd.","1-947-194-5578"),
+  (2,"Richard Benson","lorem.semper@aol.org","Ap #208-7599 Cursus Ave","1-425-544-5854"),
+  (3,"Hyatt Mcguire","nec.tempus.mauris@yahoo.net","651-7415 Libero Av.","(617) 359-2275"),
+  (4,"Damian Berger","integer.id.magna@aol.couk","Ap #831-8140 Mauris Street","(514) 448-1863"),
+  (5,"Nayda Morrison","feugiat.metus@google.com","Ap #247-664 Tellus. St.","1-133-426-3642");
+
+
+
+
