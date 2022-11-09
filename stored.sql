df4dba465503b0995ef832ec2dd79f2323fd8b3e
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-08 21:31:43 -0300

    Update stored.sql

--- stored.sql
+++ stored.sql
@@ -1,1 +1,25 @@
 
+
+use eleccionesmgp_bckp;
+
+select * from circuito_bckp
+order by inscriptos_bckp desc;
+ 
+drop procedure if exists sp_Circuitos_bckp1
+delimiter //
+
+create procedure sp_Circuitos_bckp1  (
+								 INOUT p_order varchar (50),
+                                 INOUT p_asc_desc varchar (50))
+                                 
+								 
+begin
+	
+    select * from circuito_bckp
+			 order by  @p_order, @p_asc_desc;
+
+
+
+  
+end //
+delimiter ; 
