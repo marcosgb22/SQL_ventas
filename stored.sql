c90989c8ae2935603c217978b12969efa6c21661
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-08 21:33:47 -0300

    Update stored.sql

--- stored.sql
+++ stored.sql
@@ -1,21 +1,21 @@
 
 
-use eleccionesmgp_bckp;
+use VENTAS;
 
-select * from circuito_bckp
-order by inscriptos_bckp desc;
+select * from USUARIOS
+order by NOMBRES desc;
  
-drop procedure if exists sp_Circuitos_bckp1
+drop procedure if exists BACKUP
 delimiter //
 
-create procedure sp_Circuitos_bckp1  (
-								 INOUT p_order varchar (50),
+create procedure BACKUP  (
+				 INOUT p_order varchar (50),
                                  INOUT p_asc_desc varchar (50))
                                  
 								 
 begin
 	
-    select * from circuito_bckp
+    select * from USUARIOS
 			 order by  @p_order, @p_asc_desc;
 
 
