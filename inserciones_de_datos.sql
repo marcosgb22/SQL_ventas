a6807e8a7bf83a1511bfaa214869428a480938af
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-09 21:33:51 -0300

    Update inserciones_de_datos.sql

--- inserciones_de_datos.sql
+++ inserciones_de_datos.sql
@@ -17,3 +17,32 @@ VALUES
   (4,"farmaco","farmaco"),
   (5,"sin categoria","sin cat");
 
+INSERT INTO `PROVEDORES` (`ID_PROVEDORES`,`NOMBRE`,`CORREO`,`DIRECCION`,`TELEFONO`)
+VALUES
+  (1, "MONSTER" ,"cras.vehicula@aol.net","P.O. Box 794, 2984 Aenean Road","(265) 322-8785"),
+  (2, "ICARO" ,"scelerisque.neque@google.com","P.O. Box 291, 8705 Sed Rd.","1-232-406-9338"),
+  (3, "PERSEO" ,"sit@hotmail.ca","Ap #877-4984 Et St.","(744) 616-5279"),
+  (4, "POWERADE" ,"proin.ultrices@google.couk","883-1903 Nec, St.","(879) 275-5456"),
+  (5, "ENERGY" ,"egestas.sed@aol.couk","380-5481 Lacus. Ave","(751) 265-8444");
+  
+  
+INSERT INTO `PRODUCTOS` (`ID_PRODUCTOS`,`NOMBRE`,`CLAVE`,`CODIGO`,`PRECIO`, `ID_CATEGORIAS`, `ID_PROVEDORES` )
+VALUES
+  (1,"Ullamcorper Foundation","",3015,72,1,1),
+  (2,"Amet Ante LLC","",9407,36,2,2),
+  (3,"Massa Company","",7052,46,1,2),
+  (4,"Nulla Corp.","",8039,65,1,2),
+  (5,"Gravida LLC","",2592,65,1,2);
+
+INSERT INTO `COMPRAS` (`ID_COMPRAS`,`FECHA`,`TOTAL`,`ID_PRODUCTOS`,`ID_USUARIOS`)
+VALUES
+  (1,"Aug 30, 2022",72,1,2),
+  (2,"Jul 15, 2023",36,2,3),
+  (3,"Jan 21, 2023",46,3,2),
+  (4,"Jul 7, 2022",65,4,3),
+  (5,"Apr 7, 2022",65,5,1);
+  
+  
+  
+  
+  
