7776563c0f1cc4d5be2337f548fadcb5fde0c6b0
Marcos <63947551+marcosgb22@users.noreply.github.com>
2022-11-14 23:21:17 -0300

    Update sentencias.sql

--- sentencias.sql
+++ sentencias.sql
@@ -1,1 +1,18 @@
 
+-- creacionde usuarios , operacion y editor con diferentes permisos
+
+create user operacion@localhost identified by 'passoperacion';
+
+grant select on `USUARIOS` to 'operacion@localhost';
+grant select on `CATEGORIAS` to 'operacion@localhost';
+grant select on `PROVEDORES` to 'operacion@localhost';
+grant select on `PRODUCTOS` to 'operacion@localhost';
+grant select on `COMPRAS` to 'operacion@localhost';
+
+create user editor@localhost identified by 'passeditor';
+
+grant select, update, insert on `USUARIOS` to 'operacion@localhost';
+grant select, update, insert on `CATEGORIAS` to 'operacion@localhost';
+grant select, update, insert on `PROVEDORES` to 'operacion@localhost';
+grant select, update, insert on `PRODUCTOS` to 'operacion@localhost';
+grant select, update, insert on `COMPRAS` to 'operacion@localhost';
